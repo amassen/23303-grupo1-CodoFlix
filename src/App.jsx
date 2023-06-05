38b253ff3869ceb5542143a3603138fc7338dc81
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-06-05 20:14:27 -0300

    #feat/#9-header-navbar Corrijo lugar en blanco

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { AdminPeliculas } from './pages/AdminPeliculas'
 import { Error } from './pages/Error'
 import AuthContext from './context/AuthContext'
 import './App.css'
+
 function App() {
   const { auth } = useContext(AuthContext)
 
